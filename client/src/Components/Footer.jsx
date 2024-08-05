621c67fd2ce5e8655d7e76998c56e1cf09bc2ff4
kalyan-kashaboina <kalyan.k@aifalabs.com>
2024-08-05 15:02:08 +0530

    Added linkedin in footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -20,7 +20,7 @@ export default function Footer() {
           href="#"
           className="md:text-3xl text-xl text-gray-900 dark:text-slate-50 hover:text-gray-500 dark:hover:text-slate-300"
         >
-          <BsFacebook />
+         <BsLinkedin />
         </a>
         <a
           href="#"
